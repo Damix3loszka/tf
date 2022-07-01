d6534f72f65437fc775ca49a711da24fc145ff2b
Damix3loszka <DamianDerp@gmail.com>
2022-07-01 16:35:03 +0200

    Changing ~ to homepath

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@
 namespace fs = std::filesystem;
 int main(int argc, char *argv[])
 {
-    std::string template_folder = "~/projekty/tf/";
+    std::string template_folder = "~/";
+
+    if (template_folder[0] == '~')
+    {
+        std::string user = getlogin();
+
+        if (user == "root")
+        {
+            template_folder = template_folder.replace(0, 1, "/root");
+        }
+        else
+        {
+            template_folder = template_folder.replace(0, 1, "/home/" + user);
+        }
+    }
 
     if (!fs::exists(template_folder))
     {
