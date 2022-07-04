01e5fc4fd658d6e38314252216bc71cc4d64e850
Damix3loszka <DamianDerp@gmail.com>
2022-07-04 09:45:04 +0200

    resigned from installtime init of program with cmake, added init in source file

--- main.cpp
+++ main.cpp
@@ -4,59 +4,68 @@
 #include <argh.h>
 #include <string>
 #include <fstream>
-#include <sstream>
 
 namespace fs = std::filesystem;
 
 int main(int argc, char *argv[])
 {
-    // std::string config_file = "/etc/tf.conf";
-    std::string config_file = "tf.conf";
-    std::string template_folder{};
 
+    ////init
     argh::parser parser;
     parser.add_params({"-F", "--folder", "-C", "--create"});
     parser.parse(argc, argv);
 
-    //// changing default template folder
-    if (parser({"-F", "--folder"}))
-    {
-        std::string new_template_folder = parser({"-F", "--folder"}).str();
-        std::fstream config(config_file, std::ios::out);
-        config << new_template_folder;
-        config.close();
-        return 0; ////endpoint
-    }
-    ////
+    std::string config_file_name = ".tf.conf";
+    std::string user = getlogin();
+    std::string path = "/home/" + user + "/";
+    std::string template_folder{};
+
+    // if (!fs::exists(path + config_file_name))
+    // {
+    //     std::ofstream config_file(path + config_file_name);
+    //     config_file << path + "template_folder/";
+    //     if (!fs::exists(path + "template_folder/"))
+    //     {
+    //         fs::create_directory(path + "template_folder/");
+    //     }
+    // }
 
     //// reading path to a template folder, wrap in a function later
-    std::fstream config(config_file, std::ios::in);
-    std::string line = "";
-    std::istringstream stream{};
-    getline(config, line);
-    template_folder = line;
+    std::ifstream config(path + config_file_name);
+    config >> template_folder;
     config.close();
     ////
 
     //// changing ~ to homepath, wrap it in a func later
     if (template_folder[0] == '~')
+        template_folder = template_folder.replace(0, 1, "/home/" + user);
+    if (template_folder[template_folder.size() - 1] != '/')
+        template_folder + "/";
+    ////
+
+    if (!fs::exists(template_folder))
     {
-        std::string user = getlogin();
-        if (user == "root")
+        //// changing default template folder
+        if (parser({"-F", "--folder"}))
         {
-            template_folder = template_folder.replace(0, 1, "/root");
+            std::string new_template_folder = parser({"-F", "--folder"}).str();
+            if (new_template_folder[0] == '~')
+                template_folder = template_folder.replace(0, 1, "/home/" + user);
+            if (new_template_folder[new_template_folder.size() - 1] != '/')
+                template_folder + "/";
+            std::ofstream config(path + config_file_name);
+            config << new_template_folder;
+            config.close();
+            return 0; ////endpoint
         }
         else
         {
-            template_folder = template_folder.replace(0, 1, "/home/" + user);
+            std::cerr << "No such folder " + template_folder + " exists. Use -F or --folder to set a default template folder." << std::endl;
+            return 1; // endpoint
         }
+        ////
     }
     ////
-    if (!fs::exists(template_folder))
-    {
-        std::cerr << "No such folder " + template_folder + " exists. Use -F or --folder to set a default template folder." << std::endl;
-        return 1; // endpoint
-    }
 
     //// checking for correct number of arguments
     if (parser.size() < 3 || parser.size() > 4)
@@ -68,6 +77,7 @@ int main(int argc, char *argv[])
 
     std::string extension, template_name, file_name;
 
+    //// reading template name, extension and file name
     if (parser.size() == 4)
     {
         extension = parser[1];
@@ -80,7 +90,6 @@ int main(int argc, char *argv[])
         template_name = "default";
         file_name = parser[2];
     }
-    //// reading template name, extension and file name
 
     std::string template_file = template_name + "." + extension;
     ////
@@ -99,7 +108,7 @@ int main(int argc, char *argv[])
         std::cin >> answer;
         if (answer == "y")
         {
-            std::fstream file(template_folder + template_file, std::ios::out);
+            std::ofstream file(template_folder + template_file);
             file << " ";
             file.close();
             std::cout << "Template created." << std::endl;
