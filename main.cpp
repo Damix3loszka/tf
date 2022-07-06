b23e94f82868383765483504393794a0eb848296
Damix3loszka <DamianDerp@gmail.com>
2022-07-06 16:27:36 +0200

    Patching, adding help and changed acceptable arguments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <filesystem>
 #include <unistd.h>
-#include "include/argh.h"
+#include "argh.h"
 #include <string>
 #include <fstream>
 
 namespace fs = std::filesystem;
+
+std::string help =
+    "tf is a simple command tool to create files based on templates given in a template folder.\n"
+    "Syntax:\n"
+    "1. tf EXTENSION TEMPLATE_NAME FILE_NAME\n"
+    "2. tf [-S, --set] PATH\n"
+    "3. tf [-F, --folder]\n"
+    "4. tf [-H, --help]\n"
+    "\n"
+    "1. Creates a file with name FILE_NAME and extension EXTENSION based on a template file with name TEMPLATE_NAME.\n"
+    "2. Changes path to a template folder to PATH.\n"
+    "3. Prints absolute path to the template folder.\n"
+    "4. Prints this.\n"
+    "\n"
+    "Templates are nothing but files in a form of TEMPLATE_NAME.EXTENSION.\n";
 
 void write_to_file(const std::string path, const std::string content);
 
@@ -15,107 +30,89 @@ void path_cleanup(std::string &path);
 
 int main(int argc, char *argv[])
 {
-
-    ////init
     argh::parser parser;
-    parser.add_params({"-F", "--folder"});
+    parser.add_params({"-S", "--set"});
     parser.parse(argc, argv);
 
     std::string config_file_name = ".tf.conf";
     std::string user = getlogin();
-    std::string path = "/home/" + user + "/";
+    std::string homepath = "/home/" + user + "/";
     std::string template_folder_path{};
 
-    if (!fs::exists(path + config_file_name))
+    if (!fs::exists(homepath + config_file_name))
     {
-        write_to_file(path + config_file_name, path + "template_folder/");
-        if (!fs::exists(path + "template_folder/"))
+        write_to_file(homepath + config_file_name, homepath + "template_folder/");
+        if (!fs::exists(homepath + "template_folder/"))
         {
-            fs::create_directory(path + "template_folder/");
+            fs::create_directory(homepath + "template_folder/");
         }
     }
-    ////
 
-    //// reading path to a template folder
-    template_folder_path = read_from_file(path + config_file_name);
-    ////
+    template_folder_path = read_from_file(homepath + config_file_name);
 
-    //// changing ~ to homepath, wrap it in a func later
     path_cleanup(template_folder_path);
-    ////
 
-    if (!fs::exists(template_folder_path))
-    {
-        //// changing default template folder
-        if (parser({"-F", "--folder"}))
-        {
-            std::string new_template_folder_path = parser({"-F", "--folder"}).str();
-            path_cleanup(new_template_folder_path);
-            write_to_file(path + config_file_name, new_template_folder_path);
-            return 0; ////endpoint
-        }
-        else
-        {
-            std::cerr << "No such folder " + template_folder_path + " exists. Use -F or --folder to set a default template folder." << std::endl;
-            return 1; // endpoint
-        }
-        ////
-    }
-    ////
+    std::string extension{}, template_name{}, file_name{};
+    int number_of_args = parser.size();
 
-    //// checking for correct number of arguments
-    if (parser.size() < 3 || parser.size() > 4)
-    {
-        std::cerr << "Invalid number of arguments." << std::endl;
-        return 2; // endpoint
-    }
-    ////
-
-    std::string extension, template_name, file_name;
-
-    //// reading template name, extension and file name
-    if (parser.size() == 4)
+    if (number_of_args == 4)
     {
         extension = parser[1];
         template_name = parser[2];
         file_name = parser[3];
-    }
-    else
-    {
-        extension = parser[1];
-        template_name = "default";
-        file_name = parser[2];
-    }
 
-    std::string template_file = template_name + "." + extension;
-    ////
+        std::string template_file = template_name + "." + extension;
 
-    //// copying template file to current directory
-    if (!fs::exists(template_folder_path + template_file))
-    {
-        if (template_name != "default")
+        if (!fs::exists(template_folder_path + template_file))
         {
-            std::cerr << "No template with given name." << std::endl;
+            std::cerr << "Template " + template_file + " doesn't exist." << std::endl;
             return 3; // endpoint
         }
-
-        std::cout << "Create a default template for ." + extension + "?(y or n): ";
-        std::string answer;
-        std::cin >> answer;
-        if (answer == "y")
-        {
-            std::ofstream file(template_folder_path + template_file);
-            file << " ";
-            file.close();
-            write_to_file(template_folder_path + template_file, " ");
-            std::cout << "Template created." << std::endl;
-        }
+        else
+            fs::copy(template_folder_path + template_file, "./" + file_name + "." + extension);
         return 0; // endpoint
     }
+    else if (number_of_args == 3)
+    {
+        if (parser({"-S", "--set"}))
+        {
+            std::string path_to_new_folder = parser({"-S", "--set"}).str();
+            std::string folder_absolute_path = fs::absolute(path_to_new_folder);
+            if (fs::exists(folder_absolute_path))
+            {
+                write_to_file(homepath + config_file_name, folder_absolute_path);
+                return 0; // endpoint
+            }
+            else
+            {
+                std::cerr << "No such folder " + template_folder_path + " exists. Create it first." << std::endl;
+                return 1; // endpoint
+            }
+        }
+    }
+    else if (number_of_args == 2)
+    {
+        if (parser[{"F", "folder"}])
+        {
+            std::cout << template_folder_path << std::endl;
+            return 0; // endpoint
+        }
+        else if (parser[{"H", "help"}])
+        {
+            std::cout << help << std::endl;
+            return 0; // endpoint
+        }
+        else
+        {
+            std::cerr << "Invalid arguments" << std::endl;
+            return 2;
+        }
+    }
     else
-        fs::copy(template_folder_path + template_file, "./" + file_name + "." + extension);
-
-    ////
+    {
+        std::cerr << "Invalid number of arguments." << std::endl;
+        return 2; // endpoint
+    }
     return 0; // endpoint
 }
 
