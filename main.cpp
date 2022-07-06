e2e0976b180766a00b8c758fdba230444665c9bc
Damix3loszka <DamianDerp@gmail.com>
2022-07-06 18:39:24 +0200

    Version 1.0, hopefully

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@ std::string help =
     "tf is a simple command tool to create files based on templates given in a template folder.\n"
     "Syntax:\n"
     "1. tf EXTENSION TEMPLATE_NAME FILE_NAME\n"
-    "2. tf [-S, --set] PATH\n"
-    "3. tf [-F, --folder]\n"
-    "4. tf [-H, --help]\n"
+    "2. tf {-S, --set} PATH\n"
+    "3. tf {-P, --path}\n"
+    "4. tf {-H, --help}\n"
     "\n"
     "1. Creates a file with name FILE_NAME and extension EXTENSION based on a template file with name TEMPLATE_NAME.\n"
     "2. Changes path to a template folder to PATH.\n"
     "3. Prints absolute path to the template folder.\n"
     "4. Prints this.\n"
     "\n"
-    "Templates are nothing but files in a form of TEMPLATE_NAME.EXTENSION.\n";
+    "Templates are nothing but files in a form of TEMPLATE_NAME.EXTENSION.";
 
 void write_to_file(const std::string path, const std::string content);
 
@@ -34,6 +34,7 @@ int main(int argc, char *argv[])
     parser.add_params({"-S", "--set"});
     parser.parse(argc, argv);
 
+    ////init
     std::string config_file_name = ".tf.conf";
     std::string user = getlogin();
     std::string homepath = "/home/" + user + "/";
@@ -47,15 +48,18 @@ int main(int argc, char *argv[])
             fs::create_directory(homepath + "template_folder/");
         }
     }
+    ////
 
+    ////Reading template folder path
     template_folder_path = read_from_file(homepath + config_file_name);
-
     path_cleanup(template_folder_path);
+    ////
 
+    //// Computing arguments
     std::string extension{}, template_name{}, file_name{};
-    int number_of_args = parser.size();
+    int number_of_pos_args = parser.size();
 
-    if (number_of_args == 4)
+    if (number_of_pos_args == 4)
     {
         extension = parser[1];
         template_name = parser[2];
@@ -72,47 +76,45 @@ int main(int argc, char *argv[])
             fs::copy(template_folder_path + template_file, "./" + file_name + "." + extension);
         return 0; // endpoint
     }
-    else if (number_of_args == 3)
+    else if (parser.params().size() == 1)
     {
-        if (parser({"-S", "--set"}))
+        if (parser({"-S", "--set"})) // Change template folder path
         {
-            std::string path_to_new_folder = parser({"-S", "--set"}).str();
+            fs::path path_to_new_folder = parser({"-S", "--set"}).str();
             std::string folder_absolute_path = fs::absolute(path_to_new_folder);
             if (fs::exists(folder_absolute_path))
             {
+                path_cleanup(folder_absolute_path);
                 write_to_file(homepath + config_file_name, folder_absolute_path);
+                std::cout << "Path changed" << std::endl;
                 return 0; // endpoint
             }
             else
             {
-                std::cerr << "No such folder " + template_folder_path + " exists. Create it first." << std::endl;
+                std::cerr << "No such folder " + folder_absolute_path + " exists. Create it first." << std::endl;
                 return 1; // endpoint
             }
         }
     }
-    else if (number_of_args == 2)
+    else if (parser.flags().size() == 1)
     {
-        if (parser[{"F", "folder"}])
+        if (parser[{"P", "path"}]) // Print template folder path
         {
             std::cout << template_folder_path << std::endl;
             return 0; // endpoint
         }
-        else if (parser[{"H", "help"}])
+        else if (parser[{"H", "help"}]) // Print help
         {
             std::cout << help << std::endl;
             return 0; // endpoint
         }
-        else
-        {
-            std::cerr << "Invalid arguments" << std::endl;
-            return 2;
-        }
     }
     else
     {
-        std::cerr << "Invalid number of arguments." << std::endl;
+        std::cerr << "Invalid arguments." << std::endl;
         return 2; // endpoint
     }
+    ////
     return 0; // endpoint
 }
 
@@ -141,5 +143,5 @@ void path_cleanup(std::string &path)
         path = path.replace(0, 1, "/home/" + user);
     }
     if (path[path.size() - 1] != '/')
-        path + "/";
+        path += "/";
 }
